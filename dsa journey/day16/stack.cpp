3b41a1375ce9e1e06a6e1b154900ec3034dc3b19
Ankit Kumar <91146159+Ankitkumarx@users.noreply.github.com>
2024-01-02 01:33:34 +0530

    stackctl

---
+++ dsa journey/day16/stack.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main(){
+    
+    stack<string>s;
+    s.push("love");
+    s.push("babbar");
+    s.push("kumar");
+    
+    cout <<"Top Element-->" << s.top() <<endl;
+    s.pop();
+    cout <<"Top Element-->" << s.top() <<endl;
+    cout <<"Size of Stack" << s.size() <<endl;
+    cout <<"Empty or not" << s.empty() <<endl;
+}
