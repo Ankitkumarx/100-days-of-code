3ba5e0138fa13e74ccf15ac6ee6da9b6ea19f78b
Ankit Kumar <91146159+Ankitkumarx@users.noreply.github.com>
2024-01-03 17:17:41 +0530

    chararray

---
+++ dsa journey/day19/chararray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char name[20];
+    
+    cout << "Enter your name" << endl;
+    cin >> name;
+    
+    cout << "Your name is :" ;
+    cout << name << endl;
+    return 0;
+    
+    
+    
+}
