49761edde87ca55c0642ddf8a85aade726a37bff
Ankit Kumar <91146159+Ankitkumarx@users.noreply.github.com>
2024-01-05 20:30:40 +0530

    replace

---
+++ dsa journey/day20/replacespace.cpp
@@ -0,0 +1,22 @@
+/*Problem statement
+You have been given a string 'STR' of words. You need to replace all the spaces between words with “@40”.*/
+
+#include<string>
+#include<iostream>
+using namespace std;
+string replaceSpaces(string &str){
+	string temp = "";
+    
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] == ' '){
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+        else
+        {
+            temp.push_back(str[i]);
+        }
+    }
+    return temp;
+}
