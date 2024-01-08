cb985c28e6608f516068827f07e08760686ce839
Ankitkumarx <kumar.ankit.ak@gmail.com>
2024-01-09 00:38:40 +0530

    armstrong

---
+++ dsa journey/day22/armstrong.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int sum = 0;
+    int originaln = n;
+
+    while (n > 0) {
+        int lastDigit = n % 10;
+        sum += (lastDigit * lastDigit * lastDigit);
+        n = n / 10;
+    }
+
+    if (sum == originaln) {
+        cout << "Armstrong number" << endl;
+    } else {
+        cout << "Not Armstrong number" << endl;
+    }
+
+    return 0;
+}
