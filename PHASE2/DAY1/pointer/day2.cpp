16b8fbb52eacfff2ab5a728b6ece0a96bdc181bd
Ankitkumarx <kumar.ankit.ak@gmail.com>
2024-01-21 15:01:00 +0530

    phase2

---
+++ PHASE2/DAY1/pointer/day2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //Null Pointer
+    // int *ptr = 0;
+    // cout << "The value of ptr is :" << ptr;
+    int a =10;
+    int *p=&a;
+    int **q=&p;
+    cout << &a << endl;
+    cout << p << endl;
+    cout << *q << endl;
+    return 0;
+}
